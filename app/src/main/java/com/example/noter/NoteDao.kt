8acb7460de1df7b512696a5a45373f7677008650
samrat1223 <samratganguly37@gmail.com>
2021-02-21 13:30:56 +0530

    Database and Respository layers added

--- app/src/main/java/com/example/noter/NoteDao.kt
+++ app/src/main/java/com/example/noter/NoteDao.kt
@@ -1,5 +1,6 @@
 package com.example.noter
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 
@@ -9,11 +10,12 @@ interface NoteDao {
     //Suppose you write an insert function and this function will insert a note into the table
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(note: Note)
+    //Suspend means that we want to perform insert in background thread and not in the main thread
+    suspend fun insert(note: Note)
 
     @Delete
-    fun delete(note: Note)
+    suspend fun delete(note: Note)
 
     @Query("Select * from notes_table order by id ASC ")
-    fun getAllNotes() : List<Note>
+    fun getAllNotes() : LiveData<List<Note>>
 }
